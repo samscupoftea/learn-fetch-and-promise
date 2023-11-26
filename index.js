62f79399cff1f43a828c346c990a174b9ba7de8e
sam hockaday <samuel.hockaday@gmail.com>
2023-11-26 13:10:29 +0000

    making form to hold fetch request data

--- index.js
+++ index.js
@@ -12,23 +12,47 @@
 // Write a getUser function that takes a username argument
 // It should fetch that user’s profile from "https://api.github.com/users/USERNAME_HERE
 
-function getUser(username) {
-    const user_url = `https://api.github.com/users/${username}`
-    return fetch(user_url).then((response) => response.json());
-}
-getUser("oliverjam").then((user) => console.log(user))
-.catch((error) => console.log(error));
+
 
 // Task 2 
 // Write a getRepos function that takes the Github user response object as an argument.
 // Fetch the a user using getUser, then use getRepos to fetch their repos using the repos_url property from the user object.
 // Log the array of repos.
 
-function getRepos(user){
-    return fetch("https://api.github.com/users/oliverjam/repos");
-}
+// function getRepos(user){
+//     return fetch("https://api.github.com/users/oliverjam/repos");
+// }
 
-getUser("oliverjam")
-.then(getRepos)
-.then(console.log)
-.catch(console.error);
+// getUser("oliverjam")
+// .then(getRepos)
+// .then(console.log)
+// .catch(console.error);
+
+// real world fetch
+const myForm = document.querySelector("form");
+
+myForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const name =
+
+});
+
+
+
+
+const data = { name: "oli "};
+
+fetch("https://echo.oliverjam.workers.dev/json",{
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "content-type": "application/json"},
+}
+)
+.then((response) =>{
+    if(!response.ok) throw new Error(response.status);
+    return response.json();
+})
+.then((json) => console.log(json))
+.catch((error) => console.error(error));
+
+
